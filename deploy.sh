d74b4559ed4315c950b8b09dc4e7d27fd16b0795
Salvador Lopez <salvador.lopez@eegeo.com>
2013-09-06 17:19:26 +0100

    Changed build process to work on Windows machines only since the PVRTexTool only produces good mip maps in windows. Buddy: Jonty

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ FILES=$(find ./zipped_themes/ -type f -name *.png);
 for f in $FILES;
 do 
 echo "Creating .pvr for $f"
-./lib/PVRTexToolCL -f PVRTC1_4 -m -flip y -legacypvr -i "$f";
+./lib/PVRTexToolCL.exe -f PVRTC1_4 -m -flip y -legacypvr -i "$f";
 status=$?
 if [ $status -ne 0 ]; then
   echo "failed to generate pvr for $f see teamcity log"
   exit $status
 fi
 echo "Creating .ktx for $f"
-./lib/PVRTexToolCL -f ETC1 -m -flip y -i "$f" -o "${f%.*}".ktx;
+./lib/PVRTexToolCL.exe -f ETC1 -m -flip y -i "$f" -o "${f%.*}".ktx;
 status=$?
 if [ $status -ne 0 ]; then
   echo "failed to generate ktx for $f see teamcity log"
@@ -33,6 +33,6 @@ find zipped_themes -name "*.png" -exec rm -rf {} \;
 sed -i.bak s/%VERSION%/$version/g ./zipped_themes/manifest.txt
 gzip -r ./zipped_themes
 find zipped_themes -name ".*" -exec rm -rf {} \;
-./lib/s3cmd-1.5.0-alpha1/s3cmd --verbose --add-header='Content-Encoding: gzip' --cf-invalidate --no-progress --force --recursive --acl-public --config ./lib/s3ini_poked put ./zipped_themes/  s3://eegeo-static/mobile-themes/$version/ > /dev/null
+./lib/s3cmd-1.0.1/s3cmd --verbose --add-header='Content-Encoding: gzip' --no-progress --force --recursive --acl-public --config ./lib/s3ini_poked put ./zipped_themes/  s3://eegeo-static/mobile-themes/$version/ > /dev/null
 rm -rf ./zipped_themes
 rm -f ./lib/s3ini_poked
