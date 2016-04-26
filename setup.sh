bfe0aff141d54cb0e0ca9441b4717cf9e87e3de1
nicky.rhodes <nicky.rhodes@kimbleoperations.com>
2016-04-26 15:23:54 +0100

    Fix for sporadically available ex_install script. The new location is now referred to from the script. This is https://bootstrap.pypa.io/ez_setup.py as specified at https://pypi.python.org/pypi/setuptools
    Change: Scott M @ Nicky R's workstation
    Buddy: Tim J

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python
+curl https://bootstrap.pypa.io/ez_setup.py | python
 easy_install pip==1.4.1
 pip install --upgrade -r pip_requirements
