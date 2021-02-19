73157ae8513a3f841284f21aca7223329237a821
Iain Bethune <59440061+iainbethune@users.noreply.github.com>
2021-02-19 09:28:28 +0000

    Add a theme manifest definition for Unity (#4)
    Buddy: Ali

--- local_create_version_files.sh
+++ local_create_version_files.sh
@@ -7,7 +7,9 @@ set -u
 readonly tc_landmark_tex_id=bt220
 readonly tc_indoor_mat_id=EegeoMobile_InteriorMaterials
 
+mkdir -p  ./build/landmark_textures_version
 curl "http://teamcity/httpAuth/app/rest/buildTypes/$tc_landmark_tex_id/builds/status:success/number?guest=1" > ./build/landmark_textures_version/version.txt
+mkdir -p ./build/interior_materials_version
 curl "http://teamcity/httpAuth/app/rest/buildTypes/$tc_indoor_mat_id/builds/status:success/number?guest=1" > ./build/interior_materials_version/version.txt
 
 echo "Successfully downloaded tc artifacts"
